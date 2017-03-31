47f102c611b22892ca39ea32fa336f8112d08d5d
krittayanee-k <krittayanee_k@cmu.ac.th>
2017-03-31 16:04:29 +0700

    ANGIE

---
+++ riri.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3 
+# @Author: krittayanee-k
+# @Date:   2017-03-31 16:03:41
+# @Last Modified time: 2017-03-31 16:04:04
+
+print("ANGIEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
